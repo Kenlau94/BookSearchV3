84f3c0f4c5f3cf7f43ac0352dbdb038401b8fbb8
Kenlau94 <kenlauwork94@gmail.com>
2023-11-28 23:34:07 -0500

    misspelled module

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,6 +1,6 @@
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
-modele.exports = { typeDefs, resolvers };
+module.exports = { typeDefs, resolvers };
 
 //sets and exports typedef and resolvers
