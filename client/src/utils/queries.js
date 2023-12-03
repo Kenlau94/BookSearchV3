5bc153acae7359c921be1375c70fb0f11b80dcc3
Kenlau94 <kenlauwork94@gmail.com>
2023-12-02 22:39:44 -0500

    xmen

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+//sets up the way to retieve saved data on
+//the authenticated signed in user
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
