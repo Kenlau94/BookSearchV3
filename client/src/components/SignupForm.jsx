6e2561d7741907e24dc6d1f59302d8611f9b3e0e
Kenlau94 <kenlauwork94@gmail.com>
2023-12-03 13:42:53 -0500

    imports

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -36,15 +36,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await createUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
+      console.log(data);
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
